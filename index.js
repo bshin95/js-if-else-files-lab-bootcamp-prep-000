161c3b414d1900e40636b761bb8851e402211a26
Bryan Shin <shinbryann@gmail.com>
2018-05-14 23:34:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var height = "70"
 if (height === "70") {
   console.log("The height is correct")
 }
+if (name === "Susan") {
+  console.log("The height is correct")
+}
 // Don't worry about this
 module.exports = { name, height
 }
